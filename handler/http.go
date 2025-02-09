d426b38c883f44f68db418319371750c41a5cd04
shenchangqing <shenqinpeng@126.com>
2025-02-09 18:59:42 +0800

    fix healthcheck

--- handler/http.go
+++ handler/http.go
@@ -8,16 +8,20 @@ import (
 func HealthCheck(ctx context.Context) error {
 	http.HandleFunc("/health_check", ok)
 
+	errCh := make(chan error)
+	go func() {
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			errCh <- err
+		}
+	}()
+
 	select {
 	case <-ctx.Done():
 		return ctx.Err()
-	default:
-		err := http.ListenAndServe(":8080", nil)
-		if err != nil {
-			return err
-		}
+	case err := <-errCh:
+		return err
 	}
-	return nil
 }
 
 func ok(w http.ResponseWriter, r *http.Request) {
